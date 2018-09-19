641d68b5a503499bc05faf4afac3a19ea518533a
yurii <liashenko.yurii@evnedev.com>
2018-09-19 17:57:45 +0300

    edit roles implemented

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
 console.log(100500);
+
+jQuery('.exist-roles').on('change', function(e){
+    console.log(jQuery(this).val());
+    var loc = window.location;
+    loc = loc + '&name=' + jQuery(this).val();
+    location.replace(loc);
+});
--- index.php
+++ index.php
@@ -11,18 +11,29 @@ Author URI: http://evnedev.com
 class Vendor{
 
     private $capabilities;
+    private $roles = [];
 
     public function __construct()
     {
+
+    }
+
+    private function getRoles(){
+        global $wp_roles;
+        $this->roles = $wp_roles->role_names;
+        unset($this->roles['administrator']);
+        unset($this->roles['editor']);
+        unset($this->roles['author']);
+        unset($this->roles['contributor']);
+        unset($this->roles['subscriber']);
+        unset($this->roles['customer']);
+        unset($this->roles['shop_manager']);
     }
 
     function add_new_roles()
     {
-
         $role = get_role( 'vendor' );
-
         // add a new capability
-
         if(!$role) {
             $result = add_role(
                 'vendor',
@@ -64,12 +75,6 @@ class Vendor{
 
     }
 
-    function getRoles(){
-        global $wp_roles;
-        //echo '<pre>';
-        var_dump($wp_roles->role_names);
-    }
-
     function PluginMenu()
     {
         $my_plugin_screen_name = add_menu_page(
@@ -80,20 +85,60 @@ class Vendor{
             array($this, 'RenderPage'),
             plugins_url('/img/icon.png',__DIR__)
         );
-        $this->getRoles();
-
     }
 
     public function saveParam($post){
-        var_dump($post);
+
+        $newCap = [];
+        $role = '';
+        foreach ($this->capabilities as $index => $cap){
+            $newCap[] = [$cap => false];
+        }
+
+        if(isset($post['new_name']) && !empty($post['new_name'])){
+            $result = add_role(
+                $post['new_name'],
+                __($post['new_name']),
+                $newCap
+
+            );
+            $role = get_role( $post['new_name'] );
+        }
+
+        if(isset($post['exist_name']) && !empty($post['exist_name'])){
+            $role = get_role( strtolower($post['exist_name']) );
+            foreach ($this->capabilities as $index => $cap) {
+                var_dump($cap);
+                $role->add_cap($cap, false);
+            }
+        }
+
+        //var_dump($post, $role);die();
+        if(isset($post['cap']) && ($role)){
+            foreach ($post['cap'] as $cup => $on) {
+                var_dump($cup);
+                $role->add_cap($cup, true);
+            }
+        }
+    }
+
+    private function getExistCapabilities($name){
+        return array_keys(get_role( strtolower($name) )->capabilities);
     }
 
     public function RenderPage(){
+        global $wp_roles;
+        $this->getRoles();
         $this->assetRegister();
         if(isset($_POST) && !empty($_POST)){
             $this->saveParam($_POST);
         }
         $this->getCapabilities();
+
+        if(isset($_GET['name'])) {
+            $existCap = $this->getExistCapabilities($_GET['name']);
+        }
+//var_dump($this->roles);
         ?>
         <div class='wrap'>
             <h2>Add new role</h2>
@@ -105,19 +150,42 @@ class Vendor{
                         <h1>Add new role</h1>
                     </div>
                     <div class="form-row">
-                        <button type="submit">Submit Form</button>
+                        <button type="submit" class="button button-primary button-large">Submit Form</button>
                     </div>
+                    <br/>
                     <div class="form-row">
                         <label>
                             <span>Role name</span>
-                            <input type="text" name="name" id="cap_name">
+                            <input type="text" name="new_name" id="new_name">
                         </label>
                     </div>
+
+                    <div class="form-title-row">
+                        <h1>Edit role</h1>
+                    </div>
+                    <select class="exist-roles" name="exist_name">
+                        <?php if(isset($_GET['name'])): ?>
+                            <option><?= $_GET['name'] ?></option>
+                            <option></option>
+                            <?php foreach ($this->roles as $index => $role): ?>
+                                <option><?= $role ?></option>
+                            <?php endforeach; ?>
+                        <?php else: ?>
+                            <option></option>
+                            <?php foreach ($this->roles as $index => $role): ?>
+                                <option><?= $role ?></option>
+                            <?php endforeach; ?>
+                        <?php endif;?>
+                    </select>
+
+
                     <div class="row">
+
                         <?php foreach ($this->capabilities as $index => $cap): ?>
+                            <?php $checked = in_array($cap, $existCap) ? 'checked' : '' ; ?>
                             <div class="col-6">
                                 <label>
-                                    <input type="checkbox" name="cap[<?= $cap ?>]">
+                                    <input type="checkbox" name="cap[<?= $cap ?>]" <?= $checked ?> >
                                     <span><?= $cap ?></span>
                                 </label>
                             </div>
